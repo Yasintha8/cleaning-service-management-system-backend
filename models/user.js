ac98694e7580f7be12bbae401090c7e4f8833887
Yasintha28 <yasinthachamikara28@gmail.com>
2025-04-29 21:36:07 +0530

    update

--- models/user.js
+++ models/user.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-
 const userSchema = new mongoose.Schema({
     username: { type: String, unique: true, required: true },
     password_hash: { type: String, required: true }
